adba132e6424f8c4c08df205eebc48b8c75886c7
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2018-01-13 11:26:28 -0700

    init-worker: add sudo to command

--- bin/init_worker.sh
+++ bin/init_worker.sh
@@ -11,7 +11,7 @@ echo 'ca-cert-hash:'
 read CA_CERT_HASH
 
 echo '-- Joining worker node to cluster'
-kubeadm join --token $TOKEN $MASTER_IP:$MASTER_PORT --discovery-token-ca-cert-hash sha256:$CA_CERT_HASH
+sudo kubeadm join --token $TOKEN $MASTER_IP:$MASTER_PORT --discovery-token-ca-cert-hash sha256:$CA_CERT_HASH
 
 echo '-- Installing current user\'s  kubectl config from the master'
 scp $MASTER_IP:~/.kube/config ~/.kube/config
