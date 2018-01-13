1515ecfabfc6479ac5f2461e2d03e82d4de518d0
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2018-01-13 16:34:28 -0700

    get-secrets: more typos

--- bin/get_secrets.sh
+++ bin/get_secrets.sh
@@ -7,7 +7,7 @@ hostname -i # <-- centOs
 echo "-- kubeport:"
 echo 6443 # <-- lazy, should do this dynamically
 echo "-- kubernetes tokens"
-sudo kubeadm tokens list
+sudo kubeadm token list
 echo "-- The sha256 used for the cert field when joining nodes to this master"
 # https://github.com/kubernetes/kubeadm/issues/519
 openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'
