6e351351a104f4820a8ca6daabb7e30a20d2d04f
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2018-01-12 23:03:51 -0700

    init: add install/startup of kubelet, kubectl, and kubeadm

--- bin/init_node.sh
+++ bin/init_node.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# script generated from documentation found here:
+#  https://kubernetes.io/docs/setup/independent/install-kubeadm/
+
 echo '-- Installing Docker --'
 yum install -y docker
 
@@ -10,4 +13,24 @@ cp ../etc/docker/daemon.json /etc/docker/
 echo '- enable and start docker'
 systemctl enable docker && systemctl start docker
 
+echo '-- Intsalling kubelet, kubeadm, and kubectl --'
+echo '- add kubenetes repos'
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+# Disabling SELinux by running setenforce 0 is required to allow 
+# containers to access the host filesystem, which is required by 
+# pod networks for example. You have to do this until SELinux 
+# support is improved in the kubelet.
+setenforce 0
+yum install -y kubelet kubeadm kubect
+
+echo '- enable and start kubelet'
+systemctl enable kubelet && systemctl start kubelet
 
