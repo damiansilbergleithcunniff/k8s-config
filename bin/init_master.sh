543296cc9a30c78d98545961f3c9460389388c25
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2018-01-13 09:46:10 -0700

    init-master: add flannel networking to the master init

--- bin/init_master.sh
+++ bin/init_master.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-echo '-- Init the cluster with kubeadm --'
-sudo kubeadm init
+echo '-- Init the cluster with kubeadm and flannel for the pod networ --'
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+echo '- Pass bridged IPv4 traffic to iptables’ chains, a requirement for some CNI plugins to work'
+sudo sysctl net.bridge.bridge-nf-call-iptables=1
 
 echo '- use the generated config for kubectl'
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+echo '- install flannel v0.9.1'
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+
+echo '- print out pods to verify that kube-dns pod is running'
+kubectl get pods --all-namespaces
