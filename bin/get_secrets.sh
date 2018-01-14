1c9232ceda8c07b20b4866f34a8ef08b0e728500
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2018-01-13 18:36:42 -0700

    init-worker: change the way we get join workers

--- bin/get_secrets.sh
+++ bin/get_secrets.sh
@@ -2,13 +2,21 @@
 
 echo "-== This should be run on a master ==-"
 
-echo "-- master node IP:"
-hostname -i # <-- centOs
-echo "-- kubeport:"
-echo 6443 # <-- lazy, should do this dynamically
-echo "-- kubernetes tokens"
-sudo kubeadm token list
-echo "-- The sha256 used for the cert field when joining nodes to this master"
+MASTER_IP=`hostname -i` # <-- centOs
+MASTER_PORT=6443 # <-- lazy, should do this dynamically
+TOKEN=`sudo kubeadm token list | grep default-node-token | cut -d' ' -f1`
 # https://github.com/kubernetes/kubeadm/issues/519
-openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'
+SHA256=`openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'`
 
+echo "-- master node IP: $MASTER_IP"
+echo "-- kubeport: $MASTER_PORT"
+echo "-- kubernetes token: $TOKEN"
+echo "-- cert: $SHA256"
+
+echo
+echo "-= This is the standard join command that is printed during master initialiation ="
+echo "kubeadm join --token $TOKEN $MASTER_IP:$MASTER_PORT --discovery-token-ca-cert-hash sha256:$SHA256"
+
+echo
+echo "-= Use this command from the root of the k8s-config repo to join a node to this master =-"
+echo "./bin/init_worker.sh $MASTER_IP $MASTER_PORT $TOKEN $SHA256"
--- bin/init_worker.sh
+++ bin/init_worker.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-echo '-- Collect master node information --'
-echo 'masterIP:'
-read MASTER_IP
-echo 'masterPort:'
-read MASTER_PORT
-echo 'token:'
-read TOKEN
-echo 'ca-cert-hash:'
-read CA_CERT_HASH
+MASTER_IP=$1
+MASTER_PORT=$2
+TOKEN=$3
+SHA256=$4
 
 echo '-- must be root'
 sudo -s <<EOF
 echo '-- setting network'
 sysctl net.bridge.bridge-nf-call-iptables=1
 echo '-- Joining worker node to cluster'
-kubeadm join --token $TOKEN $MASTER_IP:$MASTER_PORT --discovery-token-ca-cert-hash sha256:$CA_CERT_HASH
+kubeadm join --token $TOKEN $MASTER_IP:$MASTER_PORT --discovery-token-ca-cert-hash sha256:$SHA256
 echo 'exit root'
 EOF
 
